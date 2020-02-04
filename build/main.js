8fb860c87c3947e39f2bb23eb32af888382e9d4f
Anton Öhman <anton.ohman@hotmail.com>
2020-02-04 13:49:55 +0100

    Added the clock with non-working pointer

--- build/main.js
+++ build/main.js
@@ -1,3 +1,16 @@
 "use strict";
+var deg = 6;
+var hr = document.querySelector("#hr");
+var mn = document.querySelector("#mn");
+var sc = document.querySelector("#sc");
+setInterval(function () {
+    var day = new Date();
+    var hh = day.getHours() * 30;
+    var mn = day.getMinutes() * deg;
+    var ss = day.getSeconds() * deg;
+    hr.style.transform = "rotateZ(" + (hh + (mm / 12)) + "deg)";
+    mn.style.transform = "rotateZ(" + mm + "deg)";
+    sc.style.transform = "rotateZ(" + ss + "deg)";
+});
 console.log("test");
 //# sourceMappingURL=main.js.map
--- typescript/main.ts
+++ typescript/main.ts
@@ -1,1 +1,16 @@
+const deg:any = 6;
+const hr:any = document.querySelector("#hr");
+const mn:any = document.querySelector("#mn");
+const sc:any = document.querySelector("#sc");
+
+setInterval(() => {
+    let day:any = new Date();
+    let hh:any = day.getHours() * 30;
+    let mn:any = day.getMinutes() * deg;
+    let ss:any = day.getSeconds() * deg;
+    hr.style.transform = `rotateZ(${hh+(mm/12)}deg)`;
+    mn.style.transform = `rotateZ(${mm}deg)`;
+    sc.style.transform = `rotateZ(${ss}deg)`;
+})
+
 console.log("test")
